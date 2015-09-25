0e2e6f5b57acd1c0e7740d2726bbea027e2f9ed1
blha303 <stevensmith.ome@gmail.com>
2015-09-25 17:12:04 +0800

    Changed to module layout

--- coastfm973.py
+++ coastfm973.py
@@ -1,11 +1,15 @@
-#!/usr/bin/python2
+#!/usr/bin/env python2
 import urllib, json, sys
 
 
-def main():
+def get_data():
     artist, song = urllib.urlopen("http://www.coastlive.com.au/data.html").read().split("</strong> ")[1].strip().split(" - ")
     moreinfo = json.loads(urllib.urlopen("https://itunes.apple.com/search?" + urllib.urlencode({'term': "{} - {}".format(artist, song), 'entity': 'song'})).read())["results"][0]
-    print json.dumps({"artist_title": artist, "song_title": song, "album_title": moreinfo["collectionName"], "itunes_url": moreinfo["trackViewUrl"]})
+    return {"artist_title": artist, "song_title": song, "album_title": moreinfo["collectionName"], "itunes_url": moreinfo["trackViewUrl"]}
+    
+
+def main():
+    print json.dumps(get_data())
     return 0
 
 if __name__ == "__main__":
--- nova937.py
+++ nova937.py
@@ -1,9 +1,13 @@
-#!/usr/bin/python2
+#!/usr/bin/env python2
 import urllib, json, sys
 
 
+def get_data():
+    return json.loads(urllib.urlopen("http://prod-filesbucket-7hmmorphht20.s3-ap-southeast-2.amazonaws.com/nova-player-history/nova937-current.json").read())
+
+
 def main():
-    print urllib.urlopen("http://prod-filesbucket-7hmmorphht20.s3-ap-southeast-2.amazonaws.com/nova-player-history/nova937-current.json").read()
+    print json.dumps(get_data())
     return 0
 
 if __name__ == "__main__":
