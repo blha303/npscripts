166a6d5033e062afb34bb4fbeaf488e1fe7e6361
blha303 <stevensmith.ome@gmail.com>
2015-12-17 13:40:57 +0800

    Closes #1. Squashed commit of the following:
    commit f3682a795517d1c319fb9198d51751e8a3687d14
    Author: Nathan Blaney <nathan@blaney.xyz>
    Date:   Tue Dec 15 14:20:34 2015 +1100
    
        Update nova100.py
    
    commit d02875dacc339ccc0ae9308ff94266a5cde370d4
    Author: Nathan Blaney <nathan@blaney.xyz>
    Date:   Tue Dec 15 09:26:20 2015 +1100
    
        Update nova100.py
    
    commit e89c0f69fb17c68040c939b13dc8e26b334245d5
    Author: Nathan Blaney <nathan@blaney.xyz>
    Date:   Tue Dec 15 09:21:03 2015 +1100
    
        Added Nova100 exact same as Nova937

---
+++ nova100.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+from requests import get
+from json import dumps
+from sys import exit
+
+from util import get_itunes_info
+
+__version__ = "1.0.0"
+__author__ = "Mu5tank05 <nathan@blny.me>"
+__name__ = "Nova 100"
+__location__ = "Melbourne, Victoria, Australia"
+
+def get_data():
+    data = get("http://prod-filesbucket-7hmmorphht20.s3-ap-southeast-2.amazonaws.com/nova-player-history/nova100-current.json").json()
+    moreinfo = get_itunes_info(data["artist_title"], data["song_title"])
+    if not moreinfo:
+        return {"artistName": data["artist_title"],
+                "trackName": data["song_title"],
+                "collectionName": data["album_title"],
+                "trackViewUrl": data["itunes_url"]}
+    return moreinfo
+
+
+def main():
+    print(dumps(get_data()))
+    return 0
+
+if __name__ == "__main__":
+    exit(main())
